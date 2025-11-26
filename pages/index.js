b512c20cee90a2a4b8d73160890acd5a5f6bbe8d
Guilherme Bandeira <guilhermepb7@outlook.com>
2025-11-26 13:50:17 -0300

    Prettier Configuration and Code formatting

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Yasmin, eu te amo. Se você me ama também, faz meu cafézinho por favor</h1>
-}  
+function Home() {
+  return (
+    <h1>
+      Yasmin, eu te amo. Se você me ama também, faz meu cafézinho por favor
+    </h1>
+  );
+}
 
-export default Home 
+export default Home;
